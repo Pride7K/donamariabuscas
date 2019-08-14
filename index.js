5037c626e0fe1228912aca753ade7bd6bb458c58
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-08-14 17:08:09 -0300

    atualização na navbar,nos estilos e a adição da pagina produtos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const botao = window.document.getElementById("botaoaparecer");
 botao.addEventListener("click",aparecerdiv);
+const botao3 = window.document.getElementById("botaoaparecer3");
+botao3.addEventListener("click",aparecerdiv);
 const botao2 = window.document.getElementById("buttonDivChat");
 botao2.addEventListener("click",desaparecerdiv);
 
@@ -7,6 +9,7 @@ function aparecerdiv()
 {
     let div = window.document.getElementById("div1");
     botao.classList.add("sumir");
+    botao3.classList.add("sumir");
     div.classList.remove("sumir");
     div.classList.add("adicionar");
 }
@@ -14,6 +17,7 @@ function desaparecerdiv()
 {
     let div = window.document.getElementById("div1");
     botao.classList.remove("sumir");
+    botao3.classList.remove("sumir");
     div.classList.remove("adicionar");
     div.classList.add("sumir");
 }
