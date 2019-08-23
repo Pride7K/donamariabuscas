50863a4d09bab0a518de42a041bdd6e951a03c2a
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-08-23 15:27:55 -0300

    separação das rotas do arquivo de server principal

--- app.js
+++ app.js
@@ -6,25 +6,33 @@ const http = require("http");
 const debug = require("debug")("TCC:app");
 const app = express();
 
-const banco = new Sequelize("tccteste","root","123",
-{
-  host:"localhost",
-  dialect:"mysql"
-});
 
-banco.authenticate().then(function(){console.log("Conexao com banco OK")}).catch(function(a){console.log("Aparentemente ocorreu um erro no banco: " + a)});
+const server = http.createServer(app);
+const router = express.Router();
+
+////////////Codigo QUE   O COMPILA CODIGO PARA JSON //////////////////
 
 
 app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 
+
+/////////////////////////////////////////
+
+//////////// banco  ////////////////////
+const banco = new Sequelize("tccteste", "root", "123",
+  {
+    host: "localhost",
+    dialect: "mysql"
+  });
+
+banco.authenticate().then(function () { console.log("Conexao com banco OK") }).catch(function (a) { console.log("Aparentemente ocorreu um erro no banco: " + a) });
+
+/////////////////////////////////////////////////////
+
+//conexao com o chatbot
 const AssistantV2 = require('ibm-watson/assistant/v1');
-
-const port = NormalizarPorta(process.env.PORT || "3000");
-
-const server = http.createServer(app);
-const router =express.Router();
 
 const assistant = new AssistantV2({
   version: '2018-09-20',
@@ -32,18 +40,13 @@ const assistant = new AssistantV2({
   url: "https://gateway-syd.watsonplatform.net/assistant/api"
 });
 
-
-app.post("/add",function(req,resp)
-{
-  resp.send("nice");
-})
-
+//rota do bot
 app.post('/conversation/', (req, res) => {
   const { text, context = {} } = req.body;
 
   const params = {
     input: { text },
-    workspace_id:'f865dd60-880e-4cbd-abd7-72644ae74001',
+    workspace_id: 'f865dd60-880e-4cbd-abd7-72644ae74001',
     context,
   };
 
@@ -56,71 +59,51 @@ app.post('/conversation/', (req, res) => {
     }
   });
 });
+//////////////////////////////////////////////////////////////////
+
+
+const port = NormalizarPorta(process.env.PORT || "3000");
+
+//trazer as rotas 
+const index = require("./src/rotas/index-rota");
+const produtos = require("./src/rotas/produtos-rota");
 
 
 
-const route = router.get("/", function (req,resp,next)
-{
-  resp.status(200).send
-  ({
-    title:"Node Store API",
-    version:"0.0.1"
-  });
-});
+app.post("/add", function (req, resp) {
+  resp.send("nice");
+})
 
-const create = router.post("/",function(req,resp,next)
-{
- resp.status(201).send(req.body);
-});
-const put = router.put("/testando/:id",function(req,resp,next)
-{
-  const id = req.params.id;
-  resp.status(201).send({
-    id:id,
-    item:req.body
-  });
-});
-const delet = router.delete("/",function(req,resp,next)
-{
- resp.status(200).send(req.body);
-});
 
-app.use("/" , route)
-app.use("/products" , create)
-app.use("/testando/",put);
-app.use("/products",delet);
+app.use("/", index)
+app.use("/produtos", produtos)
+
 
 server.listen(port, () => console.log(`Rodando na porta ${port}`));
-server.on("error",TratandoErro);
-server.on("listening",onListening);
+server.on("error", TratandoErro);
+server.on("listening", onListening);
 
-function NormalizarPorta(val)
-{
-  const port = parseInt(val,10)
+function NormalizarPorta(val) {
+  const port = parseInt(val, 10)
 
-  if(isNaN(port))
-  {
+  if (isNaN(port)) {
     return val;
   }
-  if(port>=0)
-  {
+  if (port >= 0) {
     return port;
   }
   return false;
 }
 
-function TratandoErro(error)
-{
-  if(erpr.syscall !== "listen")
-  {
+function TratandoErro(error) {
+  if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof port ==="string" ?
-  "Pipe " + port :
-  "Port " + port;
+  const bind = typeof port === "string" ?
+    "Pipe " + port :
+    "Port " + port;
 
-  switch (error.code)
-  {
+  switch (error.code) {
     case "EACCES":
       console.error(bind + " necessita de mais privilégios");
       process.exit(1);
@@ -134,11 +117,10 @@ function TratandoErro(error)
   }
 }
 
-function onListening()
-{
+function onListening() {
   const addr = server.address();
   const bind = typeof addr === "string"
-  ? "pipe" + addr
-  : "port" + addr.port;
+    ? "pipe" + addr
+    : "port" + addr.port;
   debug("Listening on " + bind)
 }
