4bd39d23cf27a1d8c2e9b34f818814e4f67aded1
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-09-18 14:44:36 -0300

    md5 na senha e envio de email automatico

---
+++ config.js
@@ -0,0 +1,7 @@
+
+global.EMAIL = "Olá,<strong>{0}</strong>, seja bem vindo a dona Maria Buscas";
+
+module.exports = 
+{
+    sendgridkey:"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"
+}
