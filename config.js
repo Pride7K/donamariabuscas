99ba5be75e7a880866ebf92bd7d903d9ee9b60f0
Guilherme Santos Figueiredo <52872315+Pride7K@users.noreply.github.com>
2019-10-07 22:09:58 -0300

    att no estilo

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 
-global.EMAIL = "Olá,<strong>{0}</strong>, seja bem vindo a dona Maria Buscas";
+global.EMAIL = " Olá, <strong>{0}</strong>, seja bem vindo a dona Maria Buscas";
 
 module.exports =
-    {
+{
         sendgridkey: "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"
-    }
+}
